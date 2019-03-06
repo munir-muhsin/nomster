47fc67297e11030d17bdc79b3ee22ee3b43dd0a9
munir-muhsin <munirmuhsin@gmail.com>
2019-03-06 02:00:43 +0000

    error message styling

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,5 +1,5 @@
 class PlacesController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :create, :edit, :update]
+  before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
 
   def index
     @places = Place.all
@@ -10,10 +10,13 @@ class PlacesController < ApplicationController
   end
 
   def create
-    current_user.places.create(place_params)
+    @place = current_user.places.create(place_params)
+    if @place.valid?
     redirect_to root_path
+  else
+    render :new, status: :unprocessable_entity
   end
-
+  end
   def show
     @place = Place.find(params[:id])
   end
@@ -34,12 +37,21 @@ class PlacesController < ApplicationController
   end
 
     @place.update_attributes(place_params)
+    if @place.valid?
     redirect_to root_path
+  else
+    render :edit, status: :unprocessable_entity
+  end
   end
 
   def destroy
 
     @place = Place.find(params[:id])
+
+     if @place.user != current_user
+    return render plain: 'Not Allowed', status: :forbidden
+  end
+
     @place.destroy
     redirect_to root_path
   end
