1c78fc66cb4852d682cb77c0aa564e9f7b605aeb
munir-muhsin <munirmuhsin@gmail.com>
2019-03-10 23:29:49 +0000

    profile

--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -2,8 +2,11 @@ class NotificationMailer < ApplicationMailer
 
   default from: "no-replay@nomsterapp.com"
 
-  def comment_added
-    mail(to: "munirmuhsin@gmail.com", 
-      subject: "A comment has been added to yoru palce")
+  def comment_added(comment)
+    @place = comment.place
+    @place_owner = @place.user
+
+    mail(to: @place_owner.email, 
+      subject: "A comment has been added to  #{@place.name}")
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,5 +7,5 @@ resources :places do
     resources :comments, only: :create
     resources :photos, only: :create 
   end
-
+  resources :users, only: :show
 end
